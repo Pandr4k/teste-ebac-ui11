c4b2221aee32080db19cdc5a7b388d6dbda837fb
Pands <pandr4k@gmail.com>
2025-09-21 18:43:17 -0300

    Testes automatizados atualizados para email invalido e senha invalida

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,14 +1,40 @@
 ///<refence types="cypress"/>  
 
 describe('Funcionalidade: Login', () => {
- 
-    it('Deve fazer login com sucesso', () =>{
+
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+  
+    afterEach(() => {
+        cy.screenshot()
+    });
+    
+    it('Deve fazer login com sucesso', () =>{
+        
         cy.get('#username').type('pandora.teste@teste.com.br')
         cy.get('#password').type('teste@123')
         cy.get('.woocommerce-form > .button').click()
 
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, pandora.teste (não é pandora.teste? Sair)')
+    })
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.get('#username').type('pandora.invalido@teste.com.br')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain' , 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+        cy.get('.woocommerce-error').should('exist')
+    });
+
+     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+        cy.get('#username').type('pandora.teste@teste.com.br')
+        cy.get('#password').type('invalido')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain' , 'Erro: A senha fornecida para o e-mail pandora.teste@teste.com.br está incorreta. Perdeu a senha?')
+        cy.get('.woocommerce-error').should('exist')
     })
 
 })
