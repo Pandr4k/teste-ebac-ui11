28a960f7c59a3d138df2f2b2b1955bb685215761
Pands <pandr4k@gmail.com>
2025-09-21 16:26:38 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,14 @@
+///<refence types="cypress"/>  
+
+describe('Funcionalidade: Login', () => {
+ 
+    it('Deve fazer login com sucesso', () =>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('pandora.teste@teste.com.br')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, pandora.teste (não é pandora.teste? Sair)')
+    })
+
+})
