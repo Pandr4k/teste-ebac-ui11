9f14035286e610fa503454b159cbb87ef4090261
Pands <pandr4k@gmail.com>
2025-09-25 18:08:21 -0300

    atualização com page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -13,17 +13,21 @@ describe('Funcionalidade: Produtos', () => {
         cy.get('#tab-title-additional_information > a').should('exist')
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Zeppelin Yoga Pant'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain' , produto)
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        produtosPage.visitarProduto('Zeppelin Yoga Pant')
+        cy.get('.product_title').should('contain' , 'Zeppelin Yoga Pant')
     });
 
-    it('Deve adcionar produto ao carrinho', () => {
-        
+    it.only('Deve adcionar produto ao carrinho', () => {
+        let qntd = 5
+        produtosPage.buscarProduto('Zeppelin Yoga Pant')
+        produtosPage.addProdutoCarrinho('36', 'Blue', qntd)
+        cy.get('.woocommerce-message').should('contain' ,`${qntd} × “Zeppelin Yoga Pant” foram adicionados no seu carrinho.` )
     });
 });
